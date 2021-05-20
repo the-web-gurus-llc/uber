c802f14aec06e08d0390789118f0411863e4fe5c
Gabriel  Alao <gabrieldeveloper1@gmail.com>
2021-05-20 10:49:02 -0400

    uber

---
+++ Uber Clone/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Uber Clone
+//
+//  Created by Kavsoft on 23/04/20.
+//  Copyright © 2020 Kavsoft. All rights reserved.
+//
+
+// Import Google info.plist file to avoid errors.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        Home()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+// Views Are Kept In Separate File....
---
+++ Uber Clone/SwiftUI/BokkedView.swift
@@ -0,0 +1,21 @@
+//
+//  BokkedView.swift
+//  Uber Clone
+//
+//  Created by Balaji on 29/04/20.
+//  Copyright © 2020 Balaji. All rights reserved.
+//
+
+import SwiftUI
+
+struct BokkedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BokkedView_Previews: PreviewProvider {
+    static var previews: some View {
+        BokkedView()
+    }
+}
